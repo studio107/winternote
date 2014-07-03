108ede5a8bb18c6fc99910bddc493f8f31809e1f
antonokulov <qantus@mail.ru>
2014-07-03 10:14:39 +0400

    Fileman

--- src/js/editor.js
+++ src/js/editor.js
@@ -768,7 +768,7 @@
                 chunkSize: 1024 * 1024,
                 testChunks: false
             },
-
+            filemanUrl: '',
             disableLinkTarget: false, // hide link Target Checkbox
             disableDragAndDrop: false, // disable drag and drop event
             disableResizeEditor: false, // disable resizing editor
@@ -791,7 +791,7 @@
                 ['list', ['ul', 'ol']],
                 ['para', ['paragraph']],
                 ['table', ['table']],
-                ['insert', ['link', 'hr', 'upload']],
+                ['insert', ['link', 'hr', 'files', 'upload']],
                 ['view', ['fullscreen', 'codeview']],
                 ['help', ['help']]
             ],
@@ -1667,9 +1667,6 @@
 
             var imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
             if ($.inArray(extension, imageExtensions) != -1){
-
-                console.log($.inArray(extension, imageExtensions));
-
                 recordUndo($editable);
                 var $image = $("<img src='" + sUrl + "' />");
                 $image.css({
@@ -2453,6 +2450,30 @@
          * @param {Object} linkInfo
          * @return {Promise}
          */
+        this.showFileDialog = function ($editable, filemanUrl) {
+            return $.Deferred(function (deferred) {
+                var mmodal = $('<a></a>').attr('href', filemanUrl).mmodal({
+                    'onBeforeClose': function () {
+                        if (deferred.state() === 'pending') {
+                            deferred.reject();
+                        }
+                    }
+                });
+                $(document).on('fileman.opened',function(e, url){
+                    deferred.resolve(url);
+                    mmodal.close();
+                });
+            });
+
+        };
+
+        /**
+         * Show link dialog and set event handlers on dialog controls.
+         *
+         * @param {jQuery} $dialog
+         * @param {Object} linkInfo
+         * @return {Promise}
+         */
         this.showImageDialog = function ($editable, $dialog, imageInfo) {
             return $.Deferred(function (deferred) {
 
@@ -2552,6 +2573,20 @@
             /**
              * @param {Object} oLayoutInfo
              */
+            showFileDialog: function (oLayoutInfo) {
+                var $editable = oLayoutInfo.editable(),
+                    $editor = oLayoutInfo.editor();
+
+                var options = $editor.data('options');
+                editor.saveRange($editable);
+                dialog.showFileDialog($editable, options.filemanUrl).then(function (sLinkUrl) {
+                    editor.restoreRange($editable);
+                    editor.insertFile($editable, sLinkUrl);
+                }).fail(function () {
+                    editor.restoreRange($editable);
+                });
+            },
+
             showLinkDialog: function (oLayoutInfo) {
                 var $dialog = oLayoutInfo.dialog(),
                     $editable = oLayoutInfo.editable(),
@@ -2781,7 +2816,7 @@
                 menu.css({
                     'left': x,
                     'top': y
-                })
+                });
                 $('body').append(menu);
             },
             hideTableMenu: function(){
@@ -3483,6 +3518,13 @@
                     '</p>';
                 return html;
             },
+            files: function (lang) {
+                var button = tplIconButton('fa fa-picture-o fa-upload icon-folder', {
+                    event: 'showFileDialog',
+                    title: lang.image.image
+                });
+                return button;
+            },
             link: function (lang) {
                 return tplIconButton('fa fa-link icon-link', {
                     event: 'showLinkDialog',
@@ -3490,7 +3532,6 @@
                 });
             },
             video: function (lang) {
-                return '';
                 return tplIconButton('fa fa-youtube-play icon-play', {
                     event: 'showVideoDialog',
                     title: lang.video.video
@@ -4233,146 +4274,3 @@
         }
     });
 }));
-
-
-/*
- * Context.js
- * Copyright Jacob Kelley
- * MIT License
- */
-
-var context = context || (function () {
-
-    var options = {
-        fadeSpeed: 100,
-        filter: function ($obj) {
-            // Modify $obj, Do not return
-        },
-        above: 'auto',
-        preventDoubleContext: true,
-        compress: false
-    };
-
-    function initialize(opts) {
-
-        options = $.extend({}, options, opts);
-
-        $(document).on('click', 'html', function () {
-            $('.dropdown-context').fadeOut(options.fadeSpeed, function(){
-                $('.dropdown-context').css({display:''}).find('.drop-left').removeClass('drop-left');
-            });
-        });
-        if(options.preventDoubleContext){
-            $(document).on('contextmenu', '.dropdown-context', function (e) {
-                e.preventDefault();
-            });
-        }
-        $(document).on('mouseenter', '.dropdown-submenu', function(){
-            var $sub = $(this).find('.dropdown-context-sub:first'),
-                subWidth = $sub.width(),
-                subLeft = $sub.offset().left,
-                collision = (subWidth+subLeft) > window.innerWidth;
-            if(collision){
-                $sub.addClass('drop-left');
-            }
-        });
-
-    }
-
-    function updateOptions(opts){
-        options = $.extend({}, options, opts);
-    }
-
-    function buildMenu(data, id, subMenu) {
-        var subClass = (subMenu) ? ' dropdown-context-sub' : '',
-            compressed = options.compress ? ' compressed-context' : '',
-            $menu = $('<ul class="dropdown-menu dropdown-context' + subClass + compressed+'" id="dropdown-' + id + '"></ul>');
-        var i = 0, linkTarget = '';
-        for(i; i<data.length; i++) {
-            if (typeof data[i].divider !== 'undefined') {
-                $menu.append('<li class="divider"></li>');
-            } else if (typeof data[i].header !== 'undefined') {
-                $menu.append('<li class="nav-header">' + data[i].header + '</li>');
-            } else {
-                if (typeof data[i].href == 'undefined') {
-                    data[i].href = '#';
-                }
-                if (typeof data[i].target !== 'undefined') {
-                    linkTarget = ' target="'+data[i].target+'"';
-                }
-                if (typeof data[i].subMenu !== 'undefined') {
-                    $sub = ('<li class="dropdown-submenu"><a tabindex="-1" href="' + data[i].href + '">' + data[i].text + '</a></li>');
-                } else {
-                    $sub = $('<li><a tabindex="-1" href="' + data[i].href + '"'+linkTarget+'>' + data[i].text + '</a></li>');
-                }
-                if (typeof data[i].action !== 'undefined') {
-                    var actiond = new Date(),
-                        actionID = 'event-' + actiond.getTime() * Math.floor(Math.random()*100000),
-                        eventAction = data[i].action;
-                    $sub.find('a').attr('id', actionID);
-                    $('#' + actionID).addClass('context-event');
-                    $(document).on('click', '#' + actionID, eventAction);
-                }
-                $menu.append($sub);
-                if (typeof data[i].subMenu != 'undefined') {
-                    var subMenuData = buildMenu(data[i].subMenu, id, true);
-                    $menu.find('li:last').append(subMenuData);
-                }
-            }
-            if (typeof options.filter == 'function') {
-                options.filter($menu.find('li:last'));
-            }
-        }
-        return $menu;
-    }
-
-    function addContext(selector, data) {
-
-        var d = new Date(),
-            id = d.getTime(),
-            $menu = buildMenu(data, id);
-
-        $('body').append($menu);
-
-
-        $(document).on('contextmenu', selector, function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            $('.dropdown-context:not(.dropdown-context-sub)').hide();
-
-            $dd = $('#dropdown-' + id);
-            if (typeof options.above == 'boolean' && options.above) {
-                $dd.addClass('dropdown-context-up').css({
-                    top: e.pageY - 20 - $('#dropdown-' + id).height(),
-                    left: e.pageX - 13
-                }).fadeIn(options.fadeSpeed);
-            } else if (typeof options.above == 'string' && options.above == 'auto') {
-                $dd.removeClass('dropdown-context-up');
-                var autoH = $dd.height() + 12;
-                if ((e.pageY + autoH) > $('html').height()) {
-                    $dd.addClass('dropdown-context-up').css({
-                        top: e.pageY - 20 - autoH,
-                        left: e.pageX - 13
-                    }).fadeIn(options.fadeSpeed);
-                } else {
-                    $dd.css({
-                        top: e.pageY + 10,
-                        left: e.pageX - 13
-                    }).fadeIn(options.fadeSpeed);
-                }
-            }
-        });
-    }
-
-    function destroyContext(selector) {
-        $(document).off('contextmenu', selector).off('click', '.context-event');
-    }
-
-    return {
-        init: initialize,
-        settings: updateOptions,
-        attach: addContext,
-        destroy: destroyContext
-    };
-})();
